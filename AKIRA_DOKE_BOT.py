e79a501145510775fb7be87c5ea83fa5da3a4bd0
gouzei-kanekoe <stellaszetoss@gmail.com>
2025-02-24 22:41:42 +0800

    AKIRA_DOKE_BOT.py

--- AKIRA_DOKE_BOT.py
+++ AKIRA_DOKE_BOT.py
@@ -122,4 +122,4 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=5000)
